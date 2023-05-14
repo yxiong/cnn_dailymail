b7594d566f5ba6988dd72ad0156f211bb6dd2db1
yxiong <yxiong>
2023-05-14 03:41:58 +0000

    More baseline -- entire article

--- baseline_evaluation.py
+++ baseline_evaluation.py
@@ -11,12 +11,6 @@
 
 # COMMAND ----------
 
-# MAGIC %md ### Baseline rouge score
-# MAGIC
-# MAGIC Use the first 300 characters as the summary.
-
-# COMMAND ----------
-
 from datasets import load_dataset
 dataset = load_dataset("cnn_dailymail", "3.0.0", cache_dir="/dbfs/yxiong/huggingface/")
 
@@ -26,13 +20,42 @@ val_df = dataset["validation"].to_pandas()
 
 # COMMAND ----------
 
-val_df
+from tqdm import tqdm
+
+# COMMAND ----------
+
+# MAGIC %md ### Compute baseline results
+
+# COMMAND ----------
+
+# MAGIC %md #### First 300 characters
+
+# COMMAND ----------
+
+predictions = {}
+
+for _, row in tqdm(val_df.iterrows(), total=val_df.shape[0]):
+    predictions[row["id"]] = row["article"][:300]
+
+# COMMAND ----------
+
+# MAGIC %md #### Entire article
+
+# COMMAND ----------
+
+predictions = {}
+
+for _, row in tqdm(val_df.iterrows(), total=val_df.shape[0]):
+    predictions[row["id"]] = row["article"]
+
+# COMMAND ----------
+
+# MAGIC %md ### Compute Rogue Score
 
 # COMMAND ----------
 
 import numpy as np
 from rouge_score import rouge_scorer
-from tqdm import tqdm
 
 scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
 rouge1_scores = []
@@ -40,7 +63,7 @@ rougeL_scores = []
 
 for _, row in tqdm(val_df.iterrows(), total=val_df.shape[0]):
     target = row["highlights"]
-    prediction = row["article"][:300]
+    prediction = predictions[row["id"]]
     score = scorer.score(target=target, prediction=prediction)
     rouge1_scores.append(score["rouge1"].fmeasure)
     rougeL_scores.append(score["rougeL"].fmeasure)
